30678ec5c943ab50df880e9119e6957f38fb0dac
saudbaig456 <saudsamibaig@gmail.com>
2023-02-23 18:01:04 -0600

    initial dropdown functionality

---
+++ src/components/dropdown/dropdown.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Dropdown from './Dropdown';
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+} as ComponentMeta<typeof Dropdown>;
+
+//👇 We create a “template” of how args map to rendering
+// const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
+
+//👇 Each story then reuses that template
+// export const Primary = Template.bind({});
+// Primary.args = { backgroundColor: '#ff0', label: 'Button' };
+
+const items = [10, 20, 30];
+export const Primary = () => <Dropdown label="Age" items={items}/>
