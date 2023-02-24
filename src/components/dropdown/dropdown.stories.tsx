86eecb8ee1acdfaa36ec1f78c359ac4c4a7c82f6
saudbaig456 <saudsamibaig@gmail.com>
2023-02-23 20:13:55 -0600

    remove comments

--- src/components/dropdown/dropdown.stories.tsx
+++ src/components/dropdown/dropdown.stories.tsx
@@ -6,12 +6,5 @@ export default {
   component: Dropdown,
 } as ComponentMeta<typeof Dropdown>;
 
-//👇 We create a “template” of how args map to rendering
-// const Template: ComponentStory<typeof Dropdown> = (args) => <Dropdown {...args} />;
-
-//👇 Each story then reuses that template
-// export const Primary = Template.bind({});
-// Primary.args = { backgroundColor: '#ff0', label: 'Button' };
-
 const items = [10, 20, 30];
 export const Primary = () => <Dropdown label="Age" items={items}/>
