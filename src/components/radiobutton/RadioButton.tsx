e59f944449e96409b5c1176a6a30876fe1ac74aa
Aliah Shaira De Guzman <asndeguzman@gmail.com>
2023-03-03 20:46:30 -0600

    Implemented list component

--- src/components/radiobutton/RadioButton.tsx
+++ src/components/radiobutton/RadioButton.tsx
@@ -3,15 +3,17 @@ import Radio from '@mui/material/Radio';
 import FormControlLabel from '@mui/material/FormControlLabel';
 
 export interface RadioButtonProps {
-	disabled: boolean;
-	label: string;
-	error: boolean;
+	value?: string;
+	disabled?: boolean;
+	label?: string;
+	error?: boolean;
 	color?: string;
 	size?: 'small' | 'medium';
 	onClick?: () => void;
 }
 
 const RadioButton = ({
+	value,
 	disabled = false,
 	label,
 	error = false,
@@ -24,11 +26,7 @@ const RadioButton = ({
 		color: error ? '#C62828' : color }
 	};
 	return (
-		<FormControlLabel
-			disabled={disabled}
-			control={<Radio size={size} sx={disabled ? null : style} />}
-			label={label}
-		/>
+		<Radio value={value} size={size} sx={disabled ? null : style} />
 	);
 };
 
