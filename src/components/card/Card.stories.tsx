cefd9a5f55114c1707ba100dfa13f0e75b47ba84
Kevin Roa <kevinroadev@gmail.com>
2023-03-04 23:55:31 -0600

    Card component variants

--- src/components/card/Card.stories.tsx
+++ src/components/card/Card.stories.tsx
@@ -1,22 +1,73 @@
 import React from 'react';
 import Card from './Card';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import SearchBar from '../searchbar/SearchBar';
+import { IconButton } from '@mui/material';
+import { FilterList } from '@mui/icons-material';
 
 export default {
 	title: 'Surfaces/Card',
 	component: Card
 } as ComponentMeta<typeof Card>;
 
-const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args}></Card>;
 
 export const Default = Template.bind({});
-Default.args = { variant: 'default' };
+const defaultChildren: React.ReactNode = (
+	<div style={{ height: '200px' }}>Content goes here</div>
+);
+Default.args = {
+	variant: 'default',
+	title: 'Default',
+	children: defaultChildren
+};
 
 export const Criteria = Template.bind({});
-Criteria.args = { variant: 'criteria' };
+const criData = [
+	{ strength: 'A', description: 'Description A', document: 'AA' },
+	{ strength: 'B', description: 'Description B', document: 'BB' },
+	{ strength: 'C', description: 'Description C', document: 'CC' }
+];
+Criteria.args = {
+	variant: 'criteria',
+	title: 'Criteria',
+	criteriaData: criData,
+	style: { display: 'inline-block' }
+};
 
 export const Summary = Template.bind({});
-Summary.args = { variant: 'summary' };
+Summary.args = {
+	variant: 'summary',
+	title: 'Summary',
+	tabs: ['Initial Auth', 'Case History', 'Chart Check'],
+	children: defaultChildren,
+	style: { display: 'inline-block' }
+};
 
 export const Chart = Template.bind({});
-Chart.args = { variant: 'chart' };
+Chart.args = {
+	variant: 'chart',
+	title: 'Chart Documents',
+	children: defaultChildren,
+	style: { display: 'inline-block' }
+};
+
+export const TimeSeries = Template.bind({});
+const title: React.ReactNode = (
+	<div style={{ display: 'flex', flexDirection: 'row' }}>
+		<h4>Time Series</h4>
+		<SearchBar label="Search Stocks" variant="outlined"></SearchBar>
+	</div>
+);
+const timeTitleButtons: React.ReactNode = (
+	<IconButton size="small">
+		<FilterList />
+	</IconButton>
+);
+TimeSeries.args = {
+	variant: 'default',
+	title: title,
+	titleButtons: timeTitleButtons,
+	children: defaultChildren,
+	style: { display: '' }
+};
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,40 +1,116 @@
-import React from 'react';
-import './Card.css';
+import React, { PropsWithChildren } from 'react';
 import {
 	Card as MUICard,
 	CardContent,
 	Typography,
-	CardActions,
-	Button
+	IconButton,
+	TableContainer,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
+	SxProps
 } from '@mui/material';
+import { Print, Email } from '@mui/icons-material';
+
+import Tab from '../tab/Tab';
+import Dropdown from '../dropdown/Dropdown';
 
 export interface CardProps {
 	variant?: 'default' | 'criteria' | 'summary' | 'chart';
-	width?: string;
+	title?: string | React.ReactNode;
+	tabs?: string[];
+	titleButtons?: React.ReactNode;
+	criteriaData?: any;
+	style: SxProps;
 }
 
-const Card = ({ variant, width = 'auto' }: CardProps) => {
+const Card = (props: PropsWithChildren<CardProps>) => {
+	if (!props.variant) props.variant = 'default';
+
 	return (
-		<MUICard sx={{ width }}>
-			<CardContent>
-				<Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-					Word of the Day
-				</Typography>
-				<Typography variant="h5" component="div">
-					benevolent
-				</Typography>
-				<Typography sx={{ mb: 1.5 }} color="text.secondary">
-					adjective
-				</Typography>
-				<Typography variant="body2">
-					well meaning and kindly.
-					<br />
-					{'"a benevolent smile"'}
-				</Typography>
+		<MUICard
+			sx={{
+				minWidth: '50px',
+				minHeight: '50px',
+				...props.style
+			}}
+		>
+			<CardContent
+				sx={{
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'space-between',
+					alignItems: 'center'
+				}}
+			>
+				{props.title && [
+					<Typography variant="h6" lineHeight="20px">
+						{props.title}
+					</Typography>
+				]}
+				{props.variant === 'summary' && (
+					<IconButton size="small">
+						<Print />
+					</IconButton>
+				)}
+				{props.variant === 'chart' && (
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'row',
+							alignItems: 'center',
+							marginLeft: '40px'
+						}}
+					>
+						<Dropdown
+							label="Nursing Assessment"
+							items={['a', 'b', 'c']}
+						></Dropdown>
+						<IconButton size="small" sx={{ marginLeft: '15px' }}>
+							<Email />
+						</IconButton>
+					</div>
+				)}
+				{props.titleButtons && props.titleButtons}
 			</CardContent>
-			<CardActions>
-				<Button size="small">Learn More</Button>
-			</CardActions>
+			<hr
+				style={{
+					backgroundColor: '#d9d9d9',
+					height: '1px',
+					border: 'none',
+					margin: '0px'
+				}}
+			></hr>
+			{props.tabs && <Tab labels={props.tabs} />}
+			{props.variant === 'criteria' && (
+				<CardContent>
+					<TableContainer component={Paper}>
+						<Table size="small" aria-label="a dense table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Strength</TableCell>
+									<TableCell>Description</TableCell>
+									<TableCell>Document</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{props.criteriaData &&
+									props.criteriaData.map((row: any, i: number) => (
+										<TableRow key={i}>
+											<TableCell>{row.strength}</TableCell>
+											<TableCell>{row.description}</TableCell>
+											<TableCell>{row.document}</TableCell>
+										</TableRow>
+									))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</CardContent>
+			)}
+			<CardContent>{props.children}</CardContent>
 		</MUICard>
 	);
 };
