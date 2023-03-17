71016f848f5b7eb58f368b850d1004e5e6005a54
Kevin Roa <kevinroadev@gmail.com>
2023-03-17 15:13:08 -0500

    Restructure index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,20 @@
-import Badge from './components/badge/Badge';
-import Button from './components/button/Button';
-import Dropdown from './components/dropdown/Dropdown';
-import SearchBar from './components/searchbar/SearchBar';
-import Tab from './components/tab/Tab';
-import Toggle from './components/toggle/Toggle';
-import Checkbox from './components/checkbox/Checkbox';
-import RadioButton from './components/radiobutton/RadioButton';
-import Header from './components/header/Header';
-import Card from './components/card/Card';
-import SummaryCard from './components/card/SummaryCard';
-import ChartCard from './components/card/ChartCard';
-import CriteriaCard from './components/card/CriteriaCard';
-import List from './components/list/List';
-import Menu from './components/menu/Menu';
-import Form from './components/form/Form';
+export { default as Accordion } from './components/accordion/Accordion';
+export { default as Badge } from './components/badge/Badge';
+export { default as Button } from './components/button/Button';
+export { default as Card } from './components/card/Card';
+export { default as SummaryCard } from './components/card/SummaryCard';
+export { default as ChartCard } from './components/card/ChartCard';
+export { default as CriteriaCard } from './components/card/CriteriaCard';
+export { default as Checkbox } from './components/checkbox/Checkbox';
+export { default as Dropdown } from './components/dropdown/Dropdown';
+export { default as Form } from './components/form/Form';
+export { default as Header } from './components/header/Header';
+export { default as List } from './components/list/List';
+export { default as Menu } from './components/menu/Menu';
+export { default as RadioButton } from './components/radiobutton/RadioButton';
+export { default as SearchBar } from './components/searchbar/SearchBar';
+export { default as Tab } from './components/tab/Tab';
+export { default as TextField } from './components/textfield/TextField';
+export { default as Toggle } from './components/toggle/Toggle';
 
 import './index.css';
-
-export {
-	Badge,
-	Button,
-	Dropdown,
-	SearchBar,
-	Tab,
-	Toggle,
-	Checkbox,
-	RadioButton,
-	Header,
-	Card,
-	SummaryCard,
-	ChartCard,
-	CriteriaCard,
-	List,
-	Menu,
-	Form
-};
