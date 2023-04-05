957199cda9900b06d6327f44ee4dc2649d8c1489
Kevin Roa <kevinroadev@gmail.com>
2023-04-05 18:25:12 -0500

    Fix nested <a> in filterchip

--- src/components/filterchip/FilterChip.tsx
+++ src/components/filterchip/FilterChip.tsx
@@ -1,24 +1,33 @@
+import { useState } from 'react';
 import { Close } from '@mui/icons-material';
 import './FilterChip.css';
 
 export interface FilterChipProps {
 	value?: string;
+	enabled?: boolean;
 	onClick?: () => void;
 	onClose?: () => void;
 }
 
 const FilterChip = (props: FilterChipProps) => {
+	const [enabled, setEnabled] = useState(props.enabled || false);
+
+	const onClick = () => {
+		setEnabled(!enabled);
+		props.onClick && props.onClick();
+	};
+
 	const handleClose = (e: React.MouseEvent) => {
 		e.stopPropagation();
 		props.onClose && props.onClose();
 	};
 
 	return (
-		<a className="filterChip" onClick={props.onClick}>
+		<a className="filterChip" onClick={onClick}>
 			<p>{props.value}</p>
-			<a onClick={handleClose}>
+			<span onClick={handleClose}>
 				<Close />
-			</a>
+			</span>
 		</a>
 	);
 };
