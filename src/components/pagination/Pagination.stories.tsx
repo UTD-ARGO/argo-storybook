8d5f88695f91c32f68aa8022b7a400452b386ab8
LNP19 <lnguyenphu@gmail.com>
2024-02-26 19:40:10 -0600

    UG3-35 feat: implemented pagination component #7

---
+++ src/components/pagination/Pagination.stories.tsx
@@ -0,0 +1,17 @@
+import type { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import Pagination from "./Pagination";
+
+export default {
+  title: "Inputs/Pagination",
+  component: Pagination,
+} as ComponentMeta<typeof Pagination>;
+
+
+
+const Template: ComponentStory<typeof Pagination> = (args) => <Pagination {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+
---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,8 @@
+import { PaginationProps } from "@mui/material";
+import { Pagination as MuiPagination } from "@mui/material";
+const Pagination = (props: PaginationProps) => {
+  return <MuiPagination {...props} />;
+};
+
+export default Pagination;
+export{};
