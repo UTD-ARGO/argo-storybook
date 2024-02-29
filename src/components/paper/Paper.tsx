9206d4bc93c3f2b071150abe475f8f94c65701e4
Tim <44584257+TimothyNaumov@users.noreply.github.com>
2024-02-29 12:19:49 -0600

    refactor: removed style from paper

--- src/components/paper/Paper.tsx
+++ src/components/paper/Paper.tsx
@@ -1,14 +1,8 @@
-import { PaperProps } from '@mui/material';
-import { Paper as MuiPaper } from '@mui/material'; 
+import { PaperProps } from "@mui/material";
+import { Paper as MuiPaper } from "@mui/material";
 
-const Paper = (props: PaperProps) => { 
-  const paperStyle = {
-    width: '100px', 
-    height: '100px', 
-    padding: '16px'
-  };
-  return (
-    <MuiPaper {...props} style={paperStyle}/>);
-  };
-  
-  export default Paper;
+const Paper = (props: PaperProps) => {
+  return <MuiPaper {...props} />;
+};
+
+export default Paper;
